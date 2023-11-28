5852d4a133c3c804f02ba47230dbdfc02f545780
Talha Hamza <hamza050@umn.edu>
2023-11-28 15:26:31 -0600

    Groupwork addition

---
+++ classwork/test.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+int array[10] = {1,2,3,4,5,6,7,8,9,10};
+
+
+int* joseph = nullptr;
+int* talha = nullptr;
+int* mike = nullptr;
+int* andrew = nullptr;
+
+
+for (int i = 0; i < 10; i++)
+{
+    cout << array[i];
+}
+
+
+joseph = &array[0];
+talha = &array[9];
+mike = &array[5];
+andrew = &array[2];
+
+cout <<endl << "joseph's favourite number: " << *joseph << endl ;
+cout << "talha's favourite number: : " << *talha << endl << "mike's favourite number: ";
+cout << *mike <<endl<< "andrew's favourite number: " << *andrew<< endl << endl;
+
+joseph = talha;
+
+cout <<endl << endl << "joseph's favourite number: " << *joseph << endl ;
+cout << "talha's favourite number: : " << *talha << endl << "mike's favourite number: ";
+cout << *mike <<endl<< "andrew's favourite number: " << *andrew<< endl << endl;
+
+
+    return 0;
+}
